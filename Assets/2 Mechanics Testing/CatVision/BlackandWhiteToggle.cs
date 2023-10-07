941ae352266681f61799145c711e295a564f8f05
PyroAce64 <BlazingAce64@gmail.com>
2023-10-07 14:14:57 -0400

    Got all the code in for Cat Vision in Github
    So far the code is in with no errors but not all of its is working, trying to figure out the reason for that.

--- Assets/2 Mechanics Testing/CatVision/BlackandWhiteToggle.cs
+++ Assets/2 Mechanics Testing/CatVision/BlackandWhiteToggle.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
-public class BlackandWhiteToggle : MonoBehaviour
+public class BlackAndWhiteToggle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public PostProcessVolume postProcessVolume;
+    private ColorGrading colorGrading;
+
+    private bool isBlackAndWhite = false;
+
+    private void Start()
     {
-        
+        // Get the ColorGrading effect from the Post-Processing Volume
+        if (postProcessVolume.profile.TryGetSettings(out colorGrading))
+        {
+            // The ColorGrading component was found, you can proceed safely.
+        }
+        else
+        {
+            Debug.LogError("ColorGrading component not found in the Post-Processing Volume.");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            // Toggle between color and black & white
+            isBlackAndWhite = !isBlackAndWhite;
+
+            // Set the saturation of the ColorGrading effect
+            colorGrading.saturation.value = isBlackAndWhite ? -100 : 0; // -100 for full black & white, 0 for full color
+        }
     }
 }
