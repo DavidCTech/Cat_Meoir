364ae63244f0ce7ed8095920848b6c5c45616600
Skyler Donovan <skydon13@knights.ucf.edu>
2023-10-11 10:11:18 -0400

    starting clue door link

---
+++ Assets/2 Mechanics Testing/CatCamera/Scripts/Clue.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clue : MonoBehaviour
+{
+    public ClueImageManager clueImageManager;
+
+    public GameObject clue;
+
+    private bool doorOpen = false; 
+
+    public void Dooropen()
+    {
+        
+    }
+}
