6b16cdad00e748eb9a40ad759da84f9f8a2627a5
PyroAce64 <BlazingAce64@gmail.com>
2023-10-17 21:13:26 -0400

    Working On fixing black and white toggle for cat vision

--- Assets/2 Mechanics Testing/CatVision/BlackandWhiteToggle.cs
+++ Assets/2 Mechanics Testing/CatVision/BlackandWhiteToggle.cs
@@ -1,29 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class BlackAndWhiteToggle : MonoBehaviour
 {
-    public PostProcessVolume postProcessVolume;
-    private ColorGrading colorGrading;
-
     public GameObject player; // Assign the player GameObject in the Inspector
 
     private PlayerVisionStates playerVisionStates; // Reference to the PlayerVisionStates script
 
+    private Volume volume;
+    private FilmGrain filmGrain;
+
     private void Start()
     {
-        // Get the ColorGrading effect from the Post-Processing Volume
-        if (postProcessVolume.profile.TryGetSettings(out colorGrading))
-        {
-            // The ColorGrading component was found, you can proceed safely.
-        }
-        else
-        {
-            Debug.LogError("ColorGrading component not found in the Post-Processing Volume.");
-        }
-
         // Get the PlayerVisionStates script from the player GameObject
         playerVisionStates = player.GetComponent<PlayerVisionStates>();
 
@@ -31,11 +22,30 @@ public class BlackAndWhiteToggle : MonoBehaviour
         {
             Debug.LogError("PlayerVisionStates component not found on the player GameObject.");
         }
+
+        // Find the Volume component on this GameObject
+        volume = GetComponent<Volume>();
+
+        if (volume == null)
+        {
+            Debug.LogError("Volume component not found on this GameObject.");
+            return;
+        }
+
+        // Check if the Volume has a FilmGrain effect
+        if (volume.profile.TryGet(out filmGrain))
+        {
+            // The FilmGrain component was found, you can proceed safely.
+        }
+        else
+        {
+            Debug.LogError("FilmGrain component not found in the Volume.");
+        }
     }
 
     private void Update()
     {
-        if (player != null && playerVisionStates != null)
+        if (player != null && playerVisionStates != null && filmGrain != null)
         {
             // Check if the player is in the Vision state using the PlayerVisionStates script
             PlayerState playerState = playerVisionStates.currentState;
@@ -46,13 +56,13 @@ public class BlackAndWhiteToggle : MonoBehaviour
             // Determine whether to apply the black and white effect based on the player's state and object's layer.
             if (playerState == PlayerState.Vision && !isInNoPostProcessingLayer)
             {
-                // Set the saturation of the ColorGrading effect for black and white
-                colorGrading.saturation.value = -100; // -100 for full black & white
+                // Enable the FilmGrain effect for a black and white look
+                filmGrain.intensity.value = 1.0f; // Adjust the intensity as needed
             }
             else
             {
-                // Set the saturation back to 0 for full color
-                colorGrading.saturation.value = 0;
+                // Disable the FilmGrain effect for full color
+                filmGrain.intensity.value = 0.0f;
             }
         }
     }
