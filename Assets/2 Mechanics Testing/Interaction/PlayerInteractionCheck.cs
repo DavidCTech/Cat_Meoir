d45ddf9129e137d1661f6dc147744fcc7815134b
DavidCTech <davidholmantech@gmail.com>
2023-10-21 14:12:05 -0400

    Temp Fix

--- Assets/2 Mechanics Testing/Interaction/PlayerInteractionCheck.cs
+++ Assets/2 Mechanics Testing/Interaction/PlayerInteractionCheck.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.InputSystem;
-using static UnityEditor.Experimental.GraphView.GraphView;
+//using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class PlayerInteractionCheck : MonoBehaviour
 {
