592f76022e36335ec5256618f64642cc540cc57a
Skyler Donovan <sk772057@ucf.edu>
2023-11-18 03:21:54 -0500

    Some controller support for photo ui

---
+++ Assets/U.I_/Photo_UI_Controller.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Photo_UI_Controller : MonoBehaviour
+{
+    public GameObject photoFirstButton, photoSecondButton, photoThirdButton, photoFourthButton;
+}
