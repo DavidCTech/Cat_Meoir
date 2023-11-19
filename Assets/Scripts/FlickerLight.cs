cdb7bfc3747b09ccc5b2e87a66563476bed99d66
Peyton Paul <peyton.z.paul@gmail.com>
2023-11-18 22:52:48 -0500

    Animations, Lighting and Material Adjustment on VS

--- Assets/Scripts/FlickerLight.cs
+++ Assets/Scripts/FlickerLight.cs
@@ -4,37 +4,81 @@ using UnityEngine;
 
 public class FlickeringLight : MonoBehaviour
 {
-    
-    public Light _Light;
+    /*
+        public Light _Light;
+        public Light spotLight;
 
-    public float MinTime;
-    public float MaxTime;
-    public float Timer;
+        public float MinTime;
+        public float MaxTime;
+        public float Timer;
 
-    public AudioSource AS;
-    public AudioClip LightAudio;
+        public AudioSource AS;
+        public AudioClip LightAudio;
+        public Material lightOff, lightOn;
+        public MeshRenderer meshRenderer;
+        Renderer renderer;
+        public int materialIndex;
+        bool wasCalled;
+        bool wasCalled2;
 
-    public void Start()
-    {
-        Timer = Random.Range(MinTime, MaxTime);
-    }
-
-    
-    public void Update()
-    {
-        FlickerLight();
-    }
-
-    void FlickerLight()
-    {
-        if (Timer > 0)
-            Timer -= Time.deltaTime;
-        
-        if (Timer<=0)
+        public void Start()
         {
-            _Light.enabled = !_Light.enabled;
             Timer = Random.Range(MinTime, MaxTime);
-            AS.PlayOneShot(LightAudio);
+            meshRenderer = GetComponent<MeshRenderer>();
         }
-    }
+
+
+        public void Update()
+        {
+            FlickerLight();
+        }
+
+        void FlickerLight()
+        {
+            if (Timer > 0)
+            {
+                Timer -= Time.deltaTime;
+                wasCalled2 = false;
+
+                if (!wasCalled)
+                {
+                    wasCalled = true;
+                    ChangeToOn();
+                }
+            }
+
+            if (Timer <= 0)
+            {
+                if (_Light != null)
+                {
+                    _Light.enabled = !_Light.enabled;
+                    spotLight.enabled = !spotLight.enabled;
+                    wasCalled = false;
+
+                    if (!wasCalled2)
+                    {
+                        wasCalled2 = true;
+                        ChangeToOff();
+                        Timer = Random.Range(MinTime, MaxTime);
+                    }
+
+
+                }
+                //AS.PlayOneShot(LightAudio);
+            }
+        }
+
+        void ChangeToOff()
+        {
+            var materialsCopy = meshRenderer.materials;
+            materialsCopy[materialIndex] = lightOff;
+            meshRenderer.materials = materialsCopy;
+        }
+
+        void ChangeToOn()
+        {
+            var materialsCopy = meshRenderer.materials;
+            materialsCopy[materialIndex] = lightOn;
+            meshRenderer.materials = materialsCopy;
+        }*/
 }
