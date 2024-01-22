4a0254a48a4d4def7adaca53832287ef397a8c46
DavidCTech <davidholmantech@gmail.com>
2024-01-22 03:17:36 -0500

    Dev Tools P3
    +Added Memory Usage Toggle Display

---
+++ Assets/Scripts/Dev Tools/MemoryDisplay.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MemoryDisplay : MonoBehaviour
+{
+    public TMP_Text memoryText;
+    private bool isMemoryDisplayActive = false; // Start with the display inactive
+
+    void Start()
+    {
+        // Set the initial state of the display
+        memoryText.gameObject.SetActive(isMemoryDisplayActive);
+    }
+
+    void Update()
+    {
+        // Toggle memory display on/off with the G key
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            isMemoryDisplayActive = !isMemoryDisplayActive;
+            memoryText.gameObject.SetActive(isMemoryDisplayActive);
+        }
+
+        // Update memory display only if it's active
+        if (isMemoryDisplayActive)
+        {
+            // Get the current memory usage in megabytes
+            float memoryUsage = (float)System.GC.GetTotalMemory(false) / (1024 * 1024);
+
+            // Update the TMP text
+            memoryText.text = $"Memory Usage: {memoryUsage:F2} MB";
+        }
+    }
+}
