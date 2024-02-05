0825ee025a9fb1b42235577c0c28f6c9daf0db7a
DavidCTech <davidholmantech@gmail.com>
2024-02-04 21:13:59 -0500

    Maps Added
    Maps Override - Testing Needed
    Master Prefabs replaced

--- Assets/2 Mechanics Testing/Map/MapScene.cs
+++ Assets/2 Mechanics Testing/Map/MapScene.cs
@@ -16,14 +16,39 @@ public class MapScene : MonoBehaviour
     {
         Scene currentScene = SceneManager.GetActiveScene();
 
-        if (currentScene.name == "Mechanics")
+        if (currentScene.name == "NewHubWorld")
         {
             GameObject firstObject = sceneMaps[0];
             firstObject.SetActive(true);
         }
-        if (currentScene.name == "MapScene2")
+        if (currentScene.name == "Apartment")
         {
             GameObject firstObject = sceneMaps[1];
+            firstObject.SetActive(true);
+        }
+        if (currentScene.name == "PoliceDepartment")
+        {
+            GameObject firstObject = sceneMaps[2];
+            firstObject.SetActive(true);
+        }
+        if (currentScene.name == "BookStore")
+        {
+            GameObject firstObject = sceneMaps[3];
+            firstObject.SetActive(true);
+        }
+        if (currentScene.name == "coffeeshop")
+        {
+            GameObject firstObject = sceneMaps[4];
+            firstObject.SetActive(true);
+        }
+        if (currentScene.name == "ShoeStore")
+        {
+            GameObject firstObject = sceneMaps[5];
+            firstObject.SetActive(true);
+        }
+        if (currentScene.name == "NightClub")
+        {
+            GameObject firstObject = sceneMaps[6];
             firstObject.SetActive(true);
         }
     }
