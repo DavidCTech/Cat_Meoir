b500ad9259c81084e41337fbdaf9c994912fc1bb
DavidCTech <davidholmantech@gmail.com>
2024-02-19 01:04:04 -0500

    Quick Fix

--- Assets/U.I_/ConfirmationPopup.cs
+++ Assets/U.I_/ConfirmationPopup.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.UI;
+using UnityEngine.UI;
 using TMPro;
 
 public class ConfirmationPopup : MonoBehaviour
