27f7fd444daaf2b95ca2f16ef51269c71e936034
Peyton Paul <peyton.z.paul@gmail.com>
2024-03-05 17:36:02 -0500

    Bookstore and Accessibility

---
+++ Assets/Scripts/Accessibility/HighContrastManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HighContrastManager : MonoBehaviour
+{
+    public static HighContrastManager instance;
+
+    public HighContrastMode[] highContrastScripts;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        highContrastScripts = FindObjectsOfType<HighContrastMode>();
+    }
+
+    public void SwapMaterials()
+    {
+        foreach (HighContrastMode script in highContrastScripts)
+        {
+            script.SwapMaterials();
+        }
+    }
+}
---
+++ Assets/Scripts/Accessibility/HighContrastMode.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighContrastMode : MonoBehaviour
+{
+    public Material targetMaterial;
+    public Material[] originalMaterials;
+    private SkinnedMeshRenderer rendererComponent;
+
+    void Start()
+    {
+        rendererComponent = GetComponent<SkinnedMeshRenderer>();
+
+        if (rendererComponent != null)
+        {
+            originalMaterials = rendererComponent.materials;
+        }
+    }
+
+    public void SwapMaterials()
+    {
+        if (rendererComponent == null || originalMaterials == null)
+        {
+            Debug.Log("Error in Array");
+            return;
+        }
+
+        if (AccessibilityManager.instance.highContrastToggle.isOn)
+        {
+            Material[] newMaterials = new Material[originalMaterials.Length];
+
+            for (int i = 0; i < newMaterials.Length; i++)
+            {
+                newMaterials[i] = targetMaterial;
+            }
+            rendererComponent.materials = newMaterials;
+        }
+        else if (!AccessibilityManager.instance.highContrastToggle.isOn)
+        {
+            rendererComponent.materials = originalMaterials;
+        }
+    }
+}
