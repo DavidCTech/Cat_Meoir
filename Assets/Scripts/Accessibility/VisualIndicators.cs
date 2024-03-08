1a5613c0457014751a2d9ff5b0cc225734b3b544
Peyton Paul <peyton.z.paul@gmail.com>
2024-03-08 17:31:31 -0500

    Bookstore and Accessibility

--- Assets/Scripts/Accessibility/VisualIndicators.cs
+++ Assets/Scripts/Accessibility/VisualIndicators.cs
@@ -1,18 +1,139 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class VisualIndicators : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public InputActionAsset playerInput;
+    private InputAction catVision;
+
+    public Material newMaterial;
+    private Material[] originalMaterials;
+    public GameObject[] objectsToSwap;
+
+    public Color newColor;
+    private Color[] originalColors;
+    public Image[] imagesToSwap;
+    public TextMeshProUGUI[] textsToSwap;
+
+    private bool isSwapped = false;
+
+    void Awake()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        StoreOriginalMaterials();
+        StoreOriginalColors();
+    }
+
+    public void OnEnable()
+    {
+        playerInput.Enable();
+
+        catVision = playerInput.FindAction("CatVision");
+        catVision.performed += CatVision;
+    }
+    public void OnDisable()
+    {
+        playerInput.Disable();
+        catVision.performed -= CatVision;
+    }
+
+    public void CatVision(InputAction.CallbackContext context)
+    {
+        Debug.Log("Is Using Cat Vision");
+        SwapMaterials();
+    }
+
+    public void StoreOriginalMaterials()
+    {
+        originalMaterials = new Material[objectsToSwap.Length];
+
+        for (int i = 0; i < objectsToSwap.Length; i++)
+        {
+            Renderer renderer = objectsToSwap[i].GetComponent<Renderer>();
+
+            if (renderer != null)
+            {
+                originalMaterials[i] = renderer.sharedMaterial;
+            }
+        }
+    }
+
+    public void StoreOriginalColors()
+    {
+        originalColors = new Color[imagesToSwap.Length + textsToSwap.Length];
+
+        for (int i = 0; i < imagesToSwap.Length; i++)
+        {
+            originalColors[i] = imagesToSwap[i].color;
+        }
+
+        for (int i = 0; i < textsToSwap.Length; i++)
+        {
+            originalColors[i + imagesToSwap.Length] = textsToSwap[i].color;
+        }
+    }
+
+    public void SwapMaterials()
+    {
+        if (!isSwapped)
+        {
+            foreach (GameObject gameObject in objectsToSwap)
+            {
+                Renderer renderer = gameObject.GetComponent<Renderer>();
+
+                if (renderer != null)
+                {
+                    renderer.material = newMaterial;
+                }
+            }
+
+            foreach (Image image in imagesToSwap)
+            {
+                image.color = newColor;
+            }
+
+            foreach (TextMeshProUGUI text in textsToSwap)
+            {
+                text.color = newColor;
+            }
+
+            isSwapped = true;
+        }
+        else
+        {
+            for (int i = 0; i < objectsToSwap.Length; i++)
+            {
+                Renderer renderer = objectsToSwap[i].GetComponent<Renderer>();
+
+                if (renderer != null && originalMaterials[i] != null)
+                {
+                    renderer.material = originalMaterials[i];
+                }
+            }
+
+            int index = 0;
+
+            foreach (Image image in imagesToSwap)
+            {
+                image.color = originalColors[index];
+                index++;
+            }
+
+            foreach (TextMeshProUGUI text in textsToSwap)
+            {
+                text.color = originalColors[index];
+                index++;
+            }
+
+            isSwapped = false;
+        }
     }
 }
