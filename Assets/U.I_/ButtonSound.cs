0e88f153c4b1543fe29d59fc749e998e066aa187
Skyler Donovan <sk772057@ucf.edu>
2024-04-10 21:20:37 -0400

    Toggle sounds
    -added more to my button sound script to work with toggles too
    -sidetags have sounds and access and controller rebind have button sounds as well

--- Assets/U.I_/ButtonSound.cs
+++ Assets/U.I_/ButtonSound.cs
@@ -7,27 +7,57 @@ public class ButtonSound : MonoBehaviour
 {
     public AudioClip clickSound; 
     private Button button;
+    private Toggle toggle;
 
     void Start()
     {
-      
+        // Try to get the button component attached to this GameObject
         button = GetComponent<Button>();
 
-       
-        button.onClick.AddListener(PlayClickSound);
+        // Try to get the toggle component attached to this GameObject
+        toggle = GetComponent<Toggle>();
+
+        // Add listeners for when the button is clicked or toggle is toggled
+        if (button != null)
+        {
+            button.onClick.AddListener(PlayClickSound);
+        }
+
+        if (toggle != null)
+        {
+            toggle.onValueChanged.AddListener(PlayClickSound);
+        }
+    }
+
+    void PlayClickSound(bool value)
+    {
+        // Play sound for toggle
+        if (toggle != null)
+        {
+            PlaySound();
+        }
     }
 
     void PlayClickSound()
     {
-        
+        // Play sound for button click
+        if (button != null)
+        {
+            PlaySound();
+        }
+    }
+
+    void PlaySound()
+    {
+        // Check if a sound effect is assigned
         if (clickSound != null)
         {
-           
+            // Play the sound effect
             AudioSource.PlayClipAtPoint(clickSound, Camera.main.transform.position);
         }
         else
         {
-            Debug.LogWarning("No sound effect assigned to the button.");
+            Debug.LogWarning("No sound effect assigned to the button or toggle.");
         }
     }
 }
