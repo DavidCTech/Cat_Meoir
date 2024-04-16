c25c9212c6c442974582aa7275e2b7786dcd3ab6
Jasmine May Darman <65880499+Linocut@users.noreply.github.com>
2024-04-16 01:42:07 -0400

    Coffeeshop hierarchy change to some clues
    - added find menu
    - moved some clues into the clue section of the hierarchy
    - changed master prefab primary optional clue info for bookstore info

--- Assets/2 Mechanics Testing/Interaction/QuickFind.cs
+++ Assets/2 Mechanics Testing/Interaction/QuickFind.cs
@@ -10,17 +10,22 @@ public class QuickFind : MonoBehaviour
     void Start()
     {
 
-        //GameObject[] foundObjects = GameObject.FindGameObjectsWithTag(searchTag);
         /*
-        GameObject[] foundObjects = FindObjectOfType<AudioListener>().gameObject;
-        objects.AddRange(foundObjects);
-
-        // Optional: Print the names of the found objects
-        foreach (GameObject obj in foundObjects)
+        GameObject[] allObjects = GameObject.FindObjectsOfType<GameObject>();
+        foreach (GameObject obj in allObjects)
         {
-            Debug.Log("Found object with tag " + searchTag + ": " + obj.name);
+            if (obj.CompareTag(searchTag))
+            {
+                objects.Add(obj);
+                // Found object with the desired tag
+                Debug.Log("Found object with tag " + searchTag + ": " + obj.name);
+            }
         }
-        */
+
+        Invoke("FindAgain", 5f);
+       */
+
+        /*
         AudioSource[] foundListeners = FindObjectsOfType<AudioSource>();
 
         foreach (AudioSource listener in foundListeners)
@@ -30,11 +35,47 @@ public class QuickFind : MonoBehaviour
             // Optional: Print the names of the found objects
             Debug.Log("Found object with AudioListener: " + listener.gameObject.name);
         }
+        */
+
+        /*
+        Description[] foundDescriptions = FindObjectsOfType<Description>();
+
+        foreach (Description description in foundDescriptions)
+        {
+            objects.Add(description.gameObject);
+
+            // Optional: Print the names of the found objects
+            Debug.Log("Found object with AudioListener: " + description.gameObject.name);
+        }
+        */
+        string layerName = searchTag;
+        int layerIndex = LayerMask.NameToLayer(layerName);
+
+        GameObject[] allObjects = GameObject.FindObjectsOfType<GameObject>();
+        foreach (GameObject obj in allObjects)
+        {
+            if (obj.layer == layerIndex)
+            {
+                objects.Add(obj);
+                // Found object with the desired layer
+                Debug.Log("Found object with layer " + layerName + ": " + obj.name);
+            }
+        }
+
+    }
+    void FindAgain()
+    {
+        GameObject[] foundObjects = GameObject.FindGameObjectsWithTag(searchTag);
+
+        //GameObject[] foundObjects = FindObjectOfType<AudioListener>().gameObject;
+        objects.AddRange(foundObjects);
+        Debug.Log("Tag search: " + searchTag);
+        // Optional: Print the names of the found objects
+        foreach (GameObject obj in foundObjects)
+        {
+            objects.Add(obj);
+            Debug.Log("Found object with tag " + searchTag + ": " + obj.name);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
